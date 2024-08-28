d70ae4407df0879d59885cccf73eddd82a4639ab
gwilldan <sayhi2godswill@gmail.com>
2024-08-28 20:37:18 +0100

    fix

--- src/app/airdrop-token/page.jsx
+++ src/app/airdrop-token/page.jsx
@@ -14,6 +14,7 @@ import {
 	txComplete,
 	getSymbol,
 } from "@/web3/utils";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function AirdropToken() {
 	// // airdropTool deployed contract address
